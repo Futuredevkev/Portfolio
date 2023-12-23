752cea8c3c67327929b3d22313dcfe11f1698779
Futuredevkev <kevinsitosoyyoguadateamo1212@outlook.com>
2023-12-23 18:07:26 -0300

    a

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Hero from "./components/hero/Hero"
 import Navbar from "./components/navbar/Navbar"
 import Parallax from "./components/parallax/Parallax"
 import Portfolio from "./components/portfolio/Portfolio"
-import Techs from "./components/techs/techs"
+import Techs from "./components/techs/Techs"
 
 function App() {
 
