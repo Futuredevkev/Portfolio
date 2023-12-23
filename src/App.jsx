e433a7e9c9764bd12a9fc102a8eb666671362342
Futuredevkev <kevinsitosoyyoguadateamo1212@outlook.com>
2023-12-23 18:07:09 -0300

    aa

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./app.scss"
-import Cursor from "./components/cursor/cursor"
+import Cursor from "./components/cursor/Cursor"
 import Hero from "./components/hero/Hero"
 import Navbar from "./components/navbar/Navbar"
 import Parallax from "./components/parallax/Parallax"
